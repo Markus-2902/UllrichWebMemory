8ad7d0e3cf57f0469e682f79be685e8cbc200679
Markus Ullrich <ullrich.m04@htlwienwest.at>
2020-11-19 08:35:53 +0100

    20201119_update1

--- js/v1.js
+++ js/v1.js
@@ -1,7 +1,15 @@
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
 let c = document.querySelectorAll(".card");
 
 for (const i of c) {
-    i.addEventListener("click",function(event){
-        console.log("klick")
-    })
+    i.addEventListener("click",change)
+}
+
+
+function change() {
+    this.classList.toggle('flipped')
 }
