7bfc79e8f31d64864db220348ff316f3ff38e301
Markus Ullrich <ullrich.m04@htlwienwest.at>
2020-11-19 08:41:51 +0100

    20201119-update2

--- js/v1.js
+++ js/v1.js
@@ -1,6 +1,10 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let index = 1; index < 17; index++) {
+    const div = document.createElement('div');
+    div.className = 'card';
+
+    div.innerHTML = index;
+
+    document.getElementById('card-deck').appendChild(div);
 }
 
 let c = document.querySelectorAll(".card");
