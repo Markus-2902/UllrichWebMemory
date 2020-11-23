fbf7b5c240b59e01ceb32224a8bf63e445a1cd8e
Markus Ullrich <ullrich.m04@htlwienwest.at>
2020-11-23 20:45:44 +0100

    20201123-update7

--- js/v1.js
+++ js/v1.js
@@ -3,11 +3,30 @@
 if (flippedcard == 2) {
     (document.getElementsByClassName("flipped")).classList.toggle("flipped");
 }**/
+let HowMuchCards = 16;
 
-for (let index = 0; index < 16; index++) {
+let classen = [];
+let c = HowMuchCards/2;
+for (let index = 0; index < c; index++) {
+    let vorlage = 'flipped';
+    let classenname = vorlage+index;
+    classen.push(classenname);
+    classen.push(classenname);
+}
+console.log(Math.floor(Math.random() * 6) + 1  );
+for (let index = 0; index < HowMuchCards; index++) {
+    let verteilungsnummer = Math.floor(Math.random() * 6) + 1;
+      
+}
+
+for (let index = 0; index < HowMuchCards; index++) {
     const div = document.createElement('div');
     div.className = 'card';
 
+    for (let index = 0; index < HowMuchCards; index++) {
+        
+        
+    }
     //div.innerHTML = index%2 + 1;
     //div.innerHTML = index + 1;
     div.innerHTML = index+1;
@@ -19,15 +38,19 @@ let c = document.querySelectorAll(".card");
 
 
 
-/**function change() {
+function change() {
     this.classList.toggle('flipped')
-    this.innerHTML.toggle(this.type)
-}**/
-function change(event){
+    if (this.classList.contains('flipped')) {
+        this.innerHTML = this.type;
+    }
+    else{
+    }
+} 
+/**function change(event){
     if (!this.classList.contains('found')) {
         openCard(this);
     }
-}
+}**/
 
 function openCard(c){
     alert(c.innerHTML);
